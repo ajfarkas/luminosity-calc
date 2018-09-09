82673153bbe18942988895b8db76e5b5d380c8b8
AJ Farkas <farkas.aj@gmail.com>
2018-09-09 14:49:10 -0700

    Add support for #xxx & names

--- js/color.js
+++ js/color.js
@@ -1,3 +1,6 @@
+import calc from './luminance.js';
+import colorName from './colorNames.js';
+
 function $(node) {
 	return document.querySelector(node);
 };
@@ -6,127 +9,14 @@ function $$(node) {
 };
 
 //for invalid inputs
-function throwAlert() {
+function throwAlert(self) {
 	const p = document.createElement('p');
 	p.className = 'input-alert';
 	p.innerHTML = self.value.match(/[^#]\b\w+[^\d]\b/)
 		? 'color names not yet supported.'
 		: 'this is not a valid color format.';
-	self.parentNode.appendChild(p) ;
+	self.parentNode.appendChild(p);
 }
-
-//calculations and conversions
-const calc = {
-	//convert '#hexstr' to [r, g, b]
-	'hex2rgb' : (hex) => {
-		hex = hex.replace('#', '');
-		const hRGB = [hex.substr(0,2), hex.substr(2,2), hex.substr(4,2)],
-					rgb = [];
-
-		hRGB.forEach((h, i) => {
-			rgb[i] = parseInt(h, 16);
-		});
-		return rgb;
-	},
-
-	//convert [r,g,b] to hex str
-	'rgb2hex': (rgb) => {
-		let result = '#';
-		rgb.forEach((num) => {
-			const hex = num.toString(16);
-			//add leading zero, if necessary
-			result += ('0' + hex).substr(-2);
-		});
-		return result;
-	},
-
-	//input color as 3-element array [r, g, b]
-	'rgb2xyz': (color) => {
-		color = [color[0]/255, color[1]/255, color[2]/255];
-		const xyz = [0, 0, 0],
-					matrix = [[0.412453, 0.357580, 0.180423],
-										[0.212671, 0.715160, 0.072169],
-										[0.019334, 0.119193, 0.950227]];
-				
-		matrix.forEach((row, i) => {
-			row.forEach((cell, j) => {
-				xyz[i] += color[j] * cell * 100;
-			});
-		});
-
-		return xyz;
-	},
-
-	//input color as 3-element array [x, y, z]
-	'xyz2rgb': (color) => {
-		const rgb = [0, 0, 0],
-					matrix = [[3.240479, -1.537150, -0.498535],
-										[-0.969256, 1.875992, 0.041556],
-										[0.055648, -0.204043, 1.057311]];
-
-		matrix.forEach((row, i) => {
-			row.forEach((cell, j) => {
-				rgb[i] += Math.round(color[j] * cell) * 2.55;
-			});
-		});
-
-		return rgb;
-	},
-
-	//input color as 3-element array [x, y, z]
-	'xyz2Lab': (color) => {
-		const d65 = [95.0456, 100, 108.8754],
-					xyzN = [];
-		let [L, a, b] = [];
-		//find X/Xn, Y/Yn, Z/Zn
-		color.forEach((d, i) => {
-			xyzN[i] = d/d65[i];
-		});
-
-		const f = (t) => {
-			if (t > 0.008856) {
-				return Math.pow(t, 1/3);
-			} else {
-				return 7.787 * t + 16/116;
-			}
-		};
-
-		if (xyzN[1] > 0.008856) {
-			L = 116 * Math.pow(xyzN[1], 1/3) - 16;
-		} else {
-			L = 903.3 * xyzN[1];
-		}
-
-		a = 500 * ( f(xyzN[0]) - f(xyzN[1]) );
-		b = 200 * ( f(xyzN[1]) - f(xyzN[2]) );
-
-		return [L, a, b];
-	},
-
-	//input color as 3-element array [L, a, b]
-	'lab2xyz': (color) => {
-		const d65 = [95.0456, 100, 108.8754],
-					p = (color[0] + 16) / 116;
-		let [x, y, z] = [];
-
-		x = d65[0] * Math.pow(p + color[1] / 500, 3);
-		y = d65[1] * Math.pow(p, 3);
-		z = d65[2] * Math.pow(p - color[2] / 200, 3);
-
-		if (y/d65[1] <= 0.008856) {
-			return console.error('ERR! Y/Yn below min ('+y/d65[1]+')');
-		}
-		return [x, y, z];
-	},
-
-	'rgb2Lab': (color) => {
-		return calc.xyz2Lab(calc.rgb2xyz(color));
-	},
-
-	'lab2rgb': (color) => {
-		return calc.xyz2rgb(calc.lab2xyz(color));
-	}
-};
 
 const cPicker = $('#color-picker'),
 			cText = $('#color-text'),
@@ -208,16 +98,26 @@ function changeColor() {
 	}
 
 	function getLuminosity(node) {
-		let rgb = undefined;
+		let rgb = undefined,
+				value = node.value;
 
 		if ( $('.input-alert') ) {
 			$('.input-alert').remove();
 		};
+		
+		if ( value.match(/#[0-9a-fA-F]{3}\b/) ) {
+			// convert 3-byte hex to 6-byte
+			value = '#' + value.replace('#', '').split('').map(h => h+h).join('');
+		} else if (colorName[value]) {
+			// convert color name to hex
+			value = colorName[value];
+		}
+
 		//convert to [r,g,b] format
-		if (node.value.match(/#[0-9a-fA-F]{6}/) ) {
+		if ( value.match(/#[0-9a-fA-F]{6}/) ) {
 			//for hex colors
-			rgb = calc.hex2rgb(node.value);
-		} else if ( node.value.match(/rgba?\(\d{1,3}\,\s?\d{1,3}\,\s?\d{1,3}/) ) {
+			rgb = calc.hex2rgb(value);
+		} else if ( value.match(/rgba?\(\d{1,3}\,\s?\d{1,3}\,\s?\d{1,3}/) ) {
 			//for rgb colors
 			const color = node.value.match(/rgba?\((\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})/);
 			rgb = [+color[1], +color[2], +color[3]];
@@ -234,16 +134,16 @@ function changeColor() {
 	} 
 
 	if (self === bgPicker || self === bgText) {
-		if (getLuminosity(self) === undefined ) {
-			return throwAlert();
+		if (getLuminosity(self) === null ) {
+			return throwAlert(self);
 		}
 		$('#results-bg').setAttribute('fill', self.value);
 	} else if (self === cPicker || self === cText) {
 		//find luminosity & place color swatches
 		const L = getLuminosity(self);
 		//check for invalid inputs
-		if (L === undefined) {
-			return throwAlert();
+		if (L === null) {
+			return throwAlert(self);
 		}
 		//create log entry with color swatch
 		createVisSwatch(self.value, L);
--- js/luminance.js
+++ js/luminance.js
@@ -1,110 +1,130 @@
-module.exports = {
-  //convert '#hexstr' to [r, g, b]
-  'hex2rgb' : function(hex) {
-    hex = hex.replace('#', '')
-    var hRGB = [hex.substr(0,2), hex.substr(2,2), hex.substr(4,2)],
-        rgb = []
+const xyzLabCalc = (t) => {
+	if (t > 0.008856) {
+		return Math.pow(t, 1/3);
+	} else {
+		return 7.787 * t + 16/116;
+	}
+};
+//calculations and conversions
+const calc = {
+	/* convert '#hexstr' to [r, g, b]
+	 * @param {String} hex: String in hexadecimal format with 6 bytes
+	 * @returns {Array} of 3 rgb values
+	*/
+	'hex2rgb' : (hex) => {
+		hex = hex.replace('#', '');
+		const hRGB = [hex.substr(0,2), hex.substr(2,2), hex.substr(4,2)];
 
-    hRGB.forEach(function(h, i) {
-      rgb[i] = parseInt(h, 16)
-    })
-    return rgb
-  },
+		return hRGB.map((h) => parseInt(h, 16));
+	},
 
-  //convert [r,g,b] to hex str
-  'rgb2hex': function(rgb) {
-    var zeroHex = '0',
-        result = '#'
-    rgb.forEach(function(num) {
-      var hex = num.toString(16)
-      //add leading zero, if necessary
-      result += (zeroHex + hex).substr(-2)
-    })
-    return result
-  },
+	/* convert [r,g,b] to hex str
+	 * @param {Array} rgb: array of 3 values, 0-255
+	 * @returns {String} representing hexadecimal value of r,g,b
+	*/
+	'rgb2hex': (rgb) => {
+		let result = '#';
+		rgb.forEach((num) => {
+			const hex = num.toString(16);
+			//add leading zero, if necessary
+			result += ('0' + hex).substr(-2);
+		});
+		return result;
+	},
 
-  //input color as 3-element array [r, g, b]
-  'rgb2xyz': function(color) {
-    color = [color[0]/255, color[1]/255, color[2]/255]
-    var xyz = [0, 0, 0],
-        matrix = [[0.412453, 0.357580, 0.180423],
-                  [0.212671, 0.715160, 0.072169],
-                  [0.019334, 0.119193, 0.950227]]
-        
-    matrix.forEach(function(row, i) {
-      row.forEach(function(cell, j) {
-        xyz[i] += color[j] * cell * 100
-      })
-    })
+	/* input color as 3-element array [r, g, b]
+	 * @param {Array} color: array of 3 (r,g,b) values, 0-255
+	 * @returns {Array} of 3 values in xyz format
+	*/
+	'rgb2xyz': (color) => {
+		const colorArr = [color[0]/255, color[1]/255, color[2]/255];
+		const xyz = [0, 0, 0],
+					matrix = [[0.412453, 0.357580, 0.180423],
+										[0.212671, 0.715160, 0.072169],
+										[0.019334, 0.119193, 0.950227]];
+				
+		matrix.forEach((row, i) => {
+			row.forEach((cell, j) => {
+				xyz[i] += colorArr[j] * cell * 100;
+			});
+		});
 
-    return xyz
-  },
+		return xyz;
+	},
 
-  //input color as 3-element array [x, y, z]
-  'xyz2rgb': function(color) {
-    var rgb = [0, 0, 0],
-        matrix = [[3.240479, -1.537150, -0.498535],
-                  [-0.969256, 1.875992, 0.041556],
-                  [0.055648, -0.204043, 1.057311]]
+	/* input color as 3-element array [x, y, z]
+	 * @param {Array} color: array of 3 (x,y,z) values
+	 * @returns {Array} of 3 rgb values
+	*/
+	'xyz2rgb': (color) => {
+		const rgb = [0, 0, 0],
+					matrix = [[3.240479, -1.537150, -0.498535],
+										[-0.969256, 1.875992, 0.041556],
+										[0.055648, -0.204043, 1.057311]];
 
-    matrix.forEach(function(row, i) {
-      row.forEach(function(cell, j) {
-        rgb[i] += Math.round(color[j] * cell) * 2.55
-      })
-    })
+		matrix.forEach((row, i) => {
+			row.forEach((cell, j) => {
+				rgb[i] += Math.round(color[j] * cell) * 2.55;
+			});
+		});
 
-    return rgb
-  },
+		return rgb;
+	},
 
-  //input color as 3-element array [x, y, z]
-  'xyz2Lab': function(color) {
-    var d65 = [95.0456, 100, 108.8754],
-        xyzN = [], 
-        L, a, b
-    //find X/Xn, Y/Yn, Z/Zn
-    color.forEach(function(d, i) {
-      xyzN[i] = d/d65[i]
-    })
+	/* input color as 3-element array [x, y, z]
+	 * @param {Array} color: array of 3 (x,y,z) values
+	 * @returns {Array} of 3 L*a*b values
+	*/
+	'xyz2Lab': (color) => {
+		const d65 = [95.0456, 100, 108.8754],
+					//find X/Xn, Y/Yn, Z/Zn
+					xyzN = color.map((d, i) => d/d65[i]);
+		let [L, a, b] = [];
 
-    var f = function(t) {
-      if (t > 0.008856)
-        return Math.pow(t, 1/3)
-      else
-        return 7.787 * t + 16/116
-    }
+		if (xyzN[1] > 0.008856) {
+			L = 116 * Math.pow(xyzN[1], 1/3) - 16;
+		} else {
+			L = 903.3 * xyzN[1];
+		}
 
-    if (xyzN[1] > 0.008856)
-      L = 116 * Math.pow(xyzN[1], 1/3) - 16
-    else
-      L = 903.3 * xyzN[1]
+		a = 500 * ( xyzLabCalc(xyzN[0]) - xyzLabCalc(xyzN[1]) );
+		b = 200 * ( xyzLabCalc(xyzN[1]) - xyzLabCalc(xyzN[2]) );
 
-    a = 500 * ( f(xyzN[0]) - f(xyzN[1]) )
-    b = 200 * ( f(xyzN[1]) - f(xyzN[2]) )
+		return [L, a, b];
+	},
 
-    return [L, a, b]
-  },
+	/* input color as 3-element array [L, a, b]
+	 * @param {Array} color: array of 3 L*a*b values
+	 * @returns {Array} of 3 values in xyz format
+	*/
+	'lab2xyz': (color) => {
+		const d65 = [95.0456, 100, 108.8754],
+					p = (color[0] + 16) / 116;
+		let [x, y, z] = [];
 
-  //input color as 3-element array [L, a, b]
-  'lab2xyz': function(color) {
-    var d65 = [95.0456, 100, 108.8754],
-        p = (color[0] + 16) / 116,
-        x, y, z
+		x = d65[0] * Math.pow(p + color[1] / 500, 3);
+		y = d65[1] * Math.pow(p, 3);
+		z = d65[2] * Math.pow(p - color[2] / 200, 3);
+		// check for development error
+		if (y/d65[1] <= 0.008856) {
+			return console.error('ERR! Y/Yn below min ('+y/d65[1]+')');
+		}
+		return [x, y, z];
+	},
+	/*
+	 * @param {Array} color: array of 3 values, 0-255
+	 * @returns {Array} of 3 L*a*b values
+	*/
+	'rgb2Lab': (color) => {
+		return calc.xyz2Lab(calc.rgb2xyz(color));
+	},
+	/*
+	 * @param {Array} color: array of 3 L*a*b values
+	 * @returns {Array} of 3 rgb values
+	*/
+	'lab2rgb': (color) => {
+		return calc.xyz2rgb(calc.lab2xyz(color));
+	}
+};
 
-    x = d65[0] * Math.pow(p + color[1] / 500, 3)
-    y = d65[1] * Math.pow(p, 3)
-    z = d65[2] * Math.pow(p - color[2] / 200, 3)
-
-    if (y/d65[1] <= 0.008856) return console.error('ERR! Y/Yn below min ('+y/d65[1]+')')
-    return [x, y, z]
-  },
-
-  'rgb2Lab': function(color) {
-    return this.xyz2Lab(this.rgb2xyz(color))
-  },
-
-  'lab2rgb': function(color) {
-    return this.xyz2rgb(this.lab2xyz(color))
-  }
-
-}
-
+export default calc;
