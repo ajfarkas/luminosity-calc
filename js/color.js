9d86077aa1d020f1479db3d4c22307f0ed891470
AJ Farkas <farkas.aj@gmail.com>
2018-09-09 21:24:45 -0700

    Results: SVG -> HTML

--- js/color.js
+++ js/color.js
@@ -24,36 +24,26 @@ const cPicker = $('#color-picker'),
 			bgText = $('#bg-text'),
 			clearSVGbtn = $('#clear-svg'),
 			clearListBtn = $('#clear-list'),
+			resultsGrad = $('.result-gradient'),
 			svg = $('#svg-results'),
 			list = $('.list');
 
 function createVisSwatch(color, L) {
-	//create nodes
-	const nsURI = 'http://www.w3.org/2000/svg';
-	const g = document.createElementNS(nsURI, 'g');
-	const rect = document.createElementNS(nsURI, 'rect');
-	const text = document.createElementNS(nsURI, 'text');
-
-	//add attributes, etc.
-	g.setAttribute('class', 'result '+ color.replace(/\s|[,.-]/g, ''));
-	//create swatches
-	rect.setAttribute('x', L);
-	rect.setAttribute('y', '2.6');
-	rect.setAttribute('width', '.6');
-	rect.setAttribute('height', '5');
-	rect.setAttribute('fill', color);
-	//add text labels to swatches
-	text.setAttribute('y', L);
-	text.setAttribute('x', '-8.4');
-	text.setAttribute('dy', '.7');
-	text.setAttribute('transform', 'rotate(-90)');
-	text.setAttribute('text-anchor', 'end');
-	text.textContent = color;
-
-	//add to SVG
-	g.appendChild(rect);
-	g.appendChild(text);
-	svg.appendChild(g);
+	// create nodes
+	const container = document.createElement('div'),
+				swatch = document.createElement('div'),
+				label = document.createElement('p');
+	// update nodes
+	container.classList.add('result');
+	container.style.left = `${L}%`;
+	swatch.classList.add('result-swatch');
+	swatch.style.backgroundColor = color;
+	label.classList.add('result-label');
+	label.innerText = color;
+	// add to DOM
+	container.appendChild(swatch);
+	container.appendChild(label);
+	resultsGrad.appendChild(container);
 }
 
 function createLog(color, L) {
@@ -73,81 +63,78 @@ function createLog(color, L) {
 	list.appendChild(item);
 }
 
-function changeColor() {
-	const self = this;
-	let that = undefined;
-	//find paired input
-	if (self === cPicker) {
-		that = cText;
-	}	else if (self === cText) {
-		that = cPicker;
-	}	else if (self === bgPicker) {
-		that = bgText;
-	}	else if (self === bgText) {
-		that = bgPicker;
+function setInputDisplay(node, value) {
+	let otherNode = null;
+	//set other input to be equal
+	if (node.id.match('picker') ) {
+		otherNode = node.id.match('color') ? cText : bgText;
+		otherNode.setAttribute('placeholder', value);
+		otherNode.value = '';
+	} else if (node.id.match('text') ) {
+		otherNode = node.id.match('color') ? cPicker : bgPicker;
+		otherNode.value = value;
+	}
+}
+
+function getLuminosity(node) {
+	let rgb = undefined,
+			value = node.value;
+
+	if ( $('.input-alert') ) {
+		$('.input-alert').remove();
+	};
+	
+	if ( value.match(/#[0-9a-fA-F]{3}\b/) ) {
+		// convert 3-byte hex to 6-byte
+		value = '#' + value.replace('#', '').split('').map(h => h+h).join('');
+	} else if (colorName[value]) {
+		// convert color name to hex
+		value = colorName[value];
 	}
 
-	function setInputDisplay(node, value) {
-		//set other input to be equal
-		if (node.id.match('picker') ) {
-			that.setAttribute('placeholder', value);
-			that.value = '';
-		} else if (node.id.match('text') ) {
-			that.value = value;
-		}
+	//convert to [r,g,b] format
+	if ( value.match(/#[0-9a-fA-F]{6}/) ) {
+		//for hex colors
+		rgb = calc.hex2rgb(value);
+	} else if ( value.match(/rgba?\(\d{1,3}\,\s?\d{1,3}\,\s?\d{1,3}/) ) {
+		//for rgb colors
+		const color = node.value.match(/rgba?\((\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})/);
+		rgb = [+color[1], +color[2], +color[3]];
+		node.value = calc.rgb2hex(rgb);
 	}
+	//if user input is valid
+	if (rgb) {
+		//match color picker and text
+		setInputDisplay(node, calc.rgb2hex(rgb));
+		//return calculated luminosity
+		return calc.rgb2Lab(rgb)[0];
+	}
+	return null;
+} 
 
-	function getLuminosity(node) {
-		let rgb = undefined,
-				value = node.value;
+function changeColor(e) {
+	const el = e.currentTarget;
 
-		if ( $('.input-alert') ) {
-			$('.input-alert').remove();
-		};
-		
-		if ( value.match(/#[0-9a-fA-F]{3}\b/) ) {
-			// convert 3-byte hex to 6-byte
-			value = '#' + value.replace('#', '').split('').map(h => h+h).join('');
-		} else if (colorName[value]) {
-			// convert color name to hex
-			value = colorName[value];
+	if (el === bgPicker || el === bgText) {
+		if (getLuminosity(el) === null ) {
+			return throwAlert(el);
 		}
-
-		//convert to [r,g,b] format
-		if ( value.match(/#[0-9a-fA-F]{6}/) ) {
-			//for hex colors
-			rgb = calc.hex2rgb(value);
-		} else if ( value.match(/rgba?\(\d{1,3}\,\s?\d{1,3}\,\s?\d{1,3}/) ) {
-			//for rgb colors
-			const color = node.value.match(/rgba?\((\d{1,3})\,\s?(\d{1,3})\,\s?(\d{1,3})/);
-			rgb = [+color[1], +color[2], +color[3]];
-			node.value = calc.rgb2hex(rgb);
-		}
-		//if user input is valid
-		if (rgb) {
-			//match color picker and text
-			setInputDisplay(node, calc.rgb2hex(rgb));
-			//return calculated luminosity
-			return calc.rgb2Lab(rgb)[0];
-		}
-		return null;
-	} 
-
-	if (self === bgPicker || self === bgText) {
-		if (getLuminosity(self) === null ) {
-			return throwAlert(self);
-		}
-		$('#results-bg').setAttribute('fill', self.value);
-	} else if (self === cPicker || self === cText) {
+		const styleEl = $('#style-updates');
+		styleEl.innerText = styleEl.innerText.replace(
+			/(--contrast-bg:)([#0-9a-zA-Z]*)(;)/,
+			`$1${el.value}$3`
+		);
+		$('#results-bg').setAttribute('fill', el.value);
+	} else if (el === cPicker || el === cText) {
 		//find luminosity & place color swatches
-		const L = getLuminosity(self);
+		const L = getLuminosity(el);
 		//check for invalid inputs
 		if (L === null) {
-			return throwAlert(self);
+			return throwAlert(el);
 		}
 		//create log entry with color swatch
-		createVisSwatch(self.value, L);
-		createLog(self.value, L);
+		createVisSwatch(el.value, L);
+		createLog(el.value, L);
 	}
 
 	
@@ -162,29 +149,25 @@ function clear(els) {
 	}
 }
 
-//prevent page reload
-cText.addEventListener('keypress', (e) => { 
+function changeColorByKeyPress (e) {
 	if (e.keyCode === 13 || e.keyCode === 9) {
 		e.preventDefault();
-		changeColor.call(this);
+		changeColor(e);
 	}
-});
+}
+
+//prevent page reload
+cText.addEventListener('keypress', changeColorByKeyPress);
 //link picker and textbox, set action
 cText.addEventListener('change', changeColor);
 bgText.addEventListener('change', changeColor);
 //check if browser supports color input
 if (Modernizr.inputtypes.color) {
-	cPicker.addEventListener('change', changeColor);
+	cPicker.addEventListener('keypress', changeColorByKeyPress);
 	bgPicker.addEventListener('change', changeColor);
 } else {
 	const pickers = [cPicker, bgPicker];
 	pickers.forEach((picker) => {
-		// //create color swatch
-		// var pSwatch = document.createElement('div')
-		// pSwatch.setAttribute('class', 'swatch')
-		// pSwatch.setAttribute('style', 'background-color:white;border:2px solid gray')
-		// //replace inputs
-		// picker.parentNode.insertBefore(pSwatch, picker)
 		picker.remove();
 	});
 }
